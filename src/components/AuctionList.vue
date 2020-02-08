16372399680a065a0b392e542895f5f118a981f6
Alan Jenkins <alan.jenkins@visualsoft.co.uk>
2020-02-08 11:19:16 +0000

    Displays a placeholder hero unit when no auctions exist

--- src/components/AuctionList.vue
+++ src/components/AuctionList.vue
@@ -1,19 +1,32 @@
 <template>
   <data-auction v-slot="{ auctions, loading }">
-    <div class="auction-list">
-      <div v-if="loading">LOADING.....</div>
+    <div v-if="loading">LOADING.....</div>
 
-      <div v-else>
-        <h1 v-if="!auctions.length">No Auctions available</h1>
+    <div v-else>
+      <div v-if="!auctions.length" class="mt-6 mb-12 max-w-2xl mx-auto">
+        <ul>
+          <li v-for="(auction, _id) in auctions" :key="_id">
+            <router-link
+              :to="auction.path"
+              class="border block p-4 hover:border-gray-400 hover:bg-gray-100"
+            >
+              {{ auction.name }}
+              <AuctionStatus :auction="auction" />
+            </router-link>
+          </li>
+        </ul>
+      </div>
 
-        <div class="auction-list-item" v-for="(auction, _id) in auctions" :key="_id">
-          <router-link :to="auction.path">
-            <div>{{ auction.name }}</div>
+      <div v-else class="bg-gray-200 border-b border-gray-300 flex items-center justify-center">
+        <div class="max-w-2xl mx-auto text-center leading-loose py-12">
+          <div class="flex justify-center items-center">
+            <img src="@/assets/icons/auction.svg" width="100" />
+          </div>
 
-            <span class="auction-list-arrow glyphicon glyphicon-chevron-right"></span>
+          <h1 class="text-3xl font-thin text-center mb-4">No auctions available</h1>
+          <p>Sorry, we were unable to find any currently active auctions at this time.</p>
 
-            <AuctionStatus class="auction-list-start-date" :auction="auction" />
-          </router-link>
+          <p>Please check back in a while for updates.</p>
         </div>
       </div>
     </div>
