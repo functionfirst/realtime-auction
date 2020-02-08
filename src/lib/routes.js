32515d328268bcf544f5d1a9589cd5e6031611e4
Alan Jenkins <alan.jenkins@visualsoft.co.uk>
2020-02-08 18:42:06 +0000

    Async loads page components

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,16 +1,13 @@
-const Home = () => import('@/pages/Home')
-const Auction = () => import('@/pages/Auction')
-
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: () => import('@/pages/Home')
   },
   {
     path: '/:name/:id',
     name: 'auction',
-    component: Auction
+    component: () => import('@/pages/Auction'),
   },
   {
     path: '/login',
