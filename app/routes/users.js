7313ccb8d3ef0988a8f16c7d35ccbe8821f2df2f
Alan Jenkins <alan.jenkins@visualsoft.co.uk>
2020-02-08 19:39:38 +0000

    Applies coding standards

--- app/routes/users.js
+++ app/routes/users.js
@@ -29,8 +29,8 @@ function me(req, res) {
 };
 
 // view all users
-function list(req, res){
-	User.find({}, null, { sort: 'name'}, function(err, user) {
+function list(req, res) {
+	User.find({}, null, { sort: 'name' }, function (err, user) {
 		if (err) res.send(err);
 
 		res.json(user);
@@ -38,47 +38,47 @@ function list(req, res){
 };
 
 // create user
-function create(req, res){
+function create(req, res) {
 	// create new instance of user model
 	var user = new User();
 
 	// set the user information (from the request)
-	user.name 				= req.body.name;
-	user.pobox 				= req.body.pobox;
-	user.address 			= req.body.address;
-	user.city 				= req.body.city;
-	user.mobile 			= req.body.mobile;
-	user.password 			= req.body.password;
-	user.confirm_password 	= req.body.confirm_password;
+	user.name = req.body.name;
+	user.pobox = req.body.pobox;
+	user.address = req.body.address;
+	user.city = req.body.city;
+	user.mobile = req.body.mobile;
 	user.username = req.body.email;
+	user.password = req.body.password;
+	user.confirm_password = req.body.confirm_password;
 
 	// VALIDATION
-	if(validator.isNull(user.name)) {
+	if (validator.isNull(user.name)) {
 		return throwValidationError('Please enter your Name.', res);
 	}
 
-	if(validator.isNull(user.username)) {
+	if (validator.isNull(user.username)) {
 		return throwValidationError('Please enter your email address.', res);
 	}
 
-	if(validator.isNull(user.password)) {
+	if (validator.isNull(user.password)) {
 		return throwValidationError('Please enter a password', res);
 	}
 
-	if(validator.isNull(user.confirm_password)) {
+	if (validator.isNull(user.confirm_password)) {
 		return throwValidationError('Please confirm your password.', res);
 	}
 
-	if(!validator.equals(user.password, user.confirm_password)) {
+	if (!validator.equals(user.password, user.confirm_password)) {
 		return throwValidationError('Please ensure your passwords match.', res);
 	}
 
 	// save user and error check
-	user.save(function(err, u) {
+	user.save(function (err, u) {
 		if (err) {
 			// check for duplicate user entry
 			if (err.code == 11000) {
-				return res.json({ success : false, message : 'A User with that username already exists.'});
+				return res.json({ success: false, message: 'A User with that username already exists.' });
 			} else {
 				return res.send(err);
 			}
@@ -87,14 +87,14 @@ function create(req, res){
 		// Send email confirmation to admin
 		// sendNewUserSignup(user.name, user.username, u._id);
 
-		res.json({ success : true, message : 'User created' });
+		res.json({ success: true, message: 'User created' });
 	})
 };
 
 
 // Retrieve single user
 function view(req, res) {
-	User.findById(req.params.user_id, function(err,user) {
+	User.findById(req.params.user_id, function (err, user) {
 		if (err) res.send(err);
 
 		res.json(user);
@@ -103,30 +103,30 @@ function view(req, res) {
 
 // Update single user information
 function update(req, res) {
-	User.findById(req.params.user_id, function(err, user) {
+	User.findById(req.params.user_id, function (err, user) {
 		if (err) return res.send(err);
 
 		// Update user info if its new
 		if (req.body.name) user.name = req.body.name;
 		if (req.body.password) user.password = req.body.password;
-		if(req.body.removeImage) user.image = '';
-		
-		user.pobox 		= req.body.pobox;
-		user.address 	= req.body.address;
-		user.city 		= req.body.city;
-		user.mobile 	= req.body.mobile;
-		
+		if (req.body.removeImage) user.image = '';
+
+		user.pobox = req.body.pobox;
+		user.address = req.body.address;
+		user.city = req.body.city;
+		user.mobile = req.body.mobile;
+
 		// If we just blocked this account we need to remove all of their auctions and autobids
-		if(user.blocked !== req.body.blocked && req.body.blocked) {
+		if (user.blocked !== req.body.blocked && req.body.blocked) {
 			clearBidHistory(req.params.user_id);
 			clearAutobidHistory(req.params.user_id);
 		}
 
-		user.blocked 	= req.body.blocked;
-		user.save(function(err, user) {
+		user.blocked = req.body.blocked;
+		user.save(function (err, user) {
 			if (err) return res.send(err);
 
-			res.json({ message : "User Updated" });
+			res.json({ message: "User Updated" });
 		})
 	})
 };
@@ -139,22 +139,22 @@ function upload(req, res) {
 	var stream = fs.createReadStream(file.path);
 
 	// writefile calls putObject behind the scenes
-	s3fsImpl.writeFile(file.filename, stream).then(function(){
-		fs.unlink(file.path, function(err) {
+	s3fsImpl.writeFile(file.filename, stream).then(function () {
+		fs.unlink(file.path, function (err) {
 			if (err) return res.send(err);
 
 			// Update user with new image
-			User.findById(req.params.user_id, function(err, user) {
+			User.findById(req.params.user_id, function (err, user) {
 				if (err) return res.send(err);
 
 				if (req.file.filename) user.image = req.file.filename;
 
-				user.save(function(err, user) {
+				user.save(function (err, user) {
 					if (err) return res.send(err);
 
 					res.json({
 						message: req.file.filename + ' successfully uploaded to the server',
-						filename : req.file.filename
+						filename: req.file.filename
 					});
 				});
 			});
@@ -163,37 +163,37 @@ function upload(req, res) {
 }
 
 function clearBidHistory(userid) {
-	Auction.find({ 'bids.userid' : userid }, function(err, auctions) {
+	Auction.find({ 'bids.userid': userid }, function (err, auctions) {
 
-		auctions.forEach(function(item) {
+		auctions.forEach(function (item) {
 			// remove all bids for this user
-			item.bids = item.bids.filter(function(obj) {
-				if(obj.userid == userid) {
+			item.bids = item.bids.filter(function (obj) {
+				if (obj.userid == userid) {
 					obj.blocked = true;
 				}
 				return obj;
 			});
 
-			Auction.findByIdAndUpdate(item.id, { bids: item.bids }, {new: true}, function(err, auction){
-				if(err) console.log(err);
+			Auction.findByIdAndUpdate(item.id, { bids: item.bids }, { new: true }, function (err, auction) {
+				if (err) console.log(err);
 			});
 		})
 	});
-}	
+}
 
 function clearAutobidHistory(userid) {
-	Auction.find({ 'autobids.userid' : userid }, function(err, auctions) {
-		auctions.forEach(function(item) {
+	Auction.find({ 'autobids.userid': userid }, function (err, auctions) {
+		auctions.forEach(function (item) {
 			// remove all bids for this user
-			item.autobids = item.autobids.filter(function(obj) {
-				if(obj.userid == userid) {
+			item.autobids = item.autobids.filter(function (obj) {
+				if (obj.userid == userid) {
 					obj.blocked = true;
 				}
 				return obj;
 			});
 
-			Auction.findByIdAndUpdate(item.id, { autobids: item.autobids }, {new: true}, function(err, auction){
-				if(err) console.log(err);
+			Auction.findByIdAndUpdate(item.id, { autobids: item.autobids }, { new: true }, function (err, auction) {
+				if (err) console.log(err);
 			});
 		})
 	});
@@ -201,23 +201,23 @@ function clearAutobidHistory(userid) {
 
 function throwValidationError(msg, res) {
 	return res.json({
-		success : false,
-		message : msg
+		success: false,
+		message: msg
 	});
 }
 
 function sendNewUserSignup(name, email, uid) {
 	var uid_link = config.domain + '/users/' + uid
 	var mailOptions = {
-		from : name + " <" + email + ">",
+		from: name + " <" + email + ">",
 		to: config.email,
-		subject : name + ' - New Account Request',
-		text : 'A new user has signed up.\r\nName: ' + name + '\r\n' + uid_link,
-		html : '<p>A new user has signed up.</p><p>Name: ' + name + '</p><p><a href="' + uid_link + '">' + uid_link + '</a></p>'
+		subject: name + ' - New Account Request',
+		text: 'A new user has signed up.\r\nName: ' + name + '\r\n' + uid_link,
+		html: '<p>A new user has signed up.</p><p>Name: ' + name + '</p><p><a href="' + uid_link + '">' + uid_link + '</a></p>'
 	}
 
-	mail.sendMail(mailOptions, function(error, info) {
-		if(error) {
+	mail.sendMail(mailOptions, function (error, info) {
+		if (error) {
 			return console.log(error);
 		}
 		console.log('Message sent' + info.response);
