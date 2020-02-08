3d01ce740b3241c3edd785dfd895bba4748f1e09
Alan Jenkins <alan.jenkins@visualsoft.co.uk>
2020-02-08 09:44:22 +0000

    Coding standards updated

--- src/components/data/Auction.vue
+++ src/components/data/Auction.vue
@@ -1,30 +1,30 @@
 <script>
-// import axios from 'axios'
-import { xhrFactory } from '@/lib/xhrFactory'
-import { auctionFactory } from '@/lib/auctionFactory'
 import Auction from "@/models/auction";
+import { xhrFactory } from "@/lib/xhrFactory";
+import { auctionFactory } from "@/lib/auctionFactory";
 
 export default {
-  data () {
+  data() {
     return {
       loading: true,
       auctions: []
-    }
+    };
   },
 
-  created () {
-    auctionFactory(xhrFactory()).all().then(response => {
-      this.auctions = response.data
-      this.loading = false
-    });
+  created() {
+    auctionFactory(xhrFactory())
+      .all()
+      .then(response => {
         this.auctions = response.data.map(auction => new Auction(auction));
+        this.loading = false;
+      });
   },
 
-  render () {
+  render() {
     return this.$scopedSlots.default({
       auctions: this.auctions,
       loading: this.loading
-    })
-  }  
-}
+    });
+  }
+};
 </script>
