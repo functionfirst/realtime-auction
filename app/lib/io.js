d26e6d5ad6de624e4f63032803e5d64ba6e99b8e
Alan Jenkins <alan.jenkins@visualsoft.co.uk>
2020-02-09 13:24:45 +0000

    Refactors socket io with es6

--- app/lib/io.js
+++ app/lib/io.js
@@ -1,20 +1,15 @@
-var socket = require('socket.io');
+const socket = require('socket.io');
 
-function socketIO(server) {
-    var io = socket.listen(server);
-
-    io.on('connection', function (socket) {
-        // broadcast a bid to other users
-        socket.on('bid:send', function (data) {
-            socket.broadcast.emit('bid:send', data);
-        });
-
-        // broadcast a bid to other users
-        socket.on('bid:reset', function () {
-            socket.broadcast.emit('bid:reset');
-        });
+const socketEvents = socket => {
+    // broadcasts a bid to other users
+    socket.on('bid:send', function (data) {
+        socket.broadcast.emit('bid:send', data);
     });
+}
 
+const socketIO = server => {
+    let io = socket.listen(server);
+    io.on('connection', socketEvents);
     return io;
 }
 
