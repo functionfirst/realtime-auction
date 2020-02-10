0a5a174c4f1af5edb2efdcb59d2e885ad32e1035
Alan Jenkins <alan.jenkins@visualsoft.co.uk>
2020-02-10 08:00:49 +0000

    Refactors auctions route
    This pushes a lot of code up onto the mode. Creating an addBid method where much of the validation occurs when a user attempts to add a bid.
    
    In this case any errors encountered are thrown as errors which are returned to the calling route.
    
    The route is then able to use try catch to return the error as an API response when the async/await call fails.

--- app/routes/auctions.js
+++ app/routes/auctions.js
@@ -1,7 +1,5 @@
-const async = require('async');
 const Auction = require('../models/auction');
 const User = require('../models/user');
-const filters = require('../lib/filters.js');
 
 const list = async (req, res) => {
   const fields = 'name description start_date end_date start_amount current_bid countdown _id';
@@ -87,117 +85,61 @@ function update(req, res) {
   });
 };
 
-function bid(req, res) {
-  var msg,
-    msgResult = true;
+const isUserBlocked = async (userid) => {
+  try {
+    const user = await User.findById(userid, 'id blocked');
+    return user && user.blocked;
+  } catch (err) {
+    return err
+  }
+}
 
-  async.series({
-
-    // Check if User is blocked
-    isUserBlocked: function (next) {
-      User.findById(req.body.userid, 'id blocked', function (err, user) {
-        if (err) res.send(err);
-
-        if (user) {
-          if (user.isBlocked()) {
-            return res.send({
-              success: false,
-              message: "Your account has been blocked."
-            });
-          }
-        }
-        next();
       });
-    },
 
-    processAuction: function (next) {
-      Auction.findById(req.params.auction_id, 'id start_date start_amount bids autobids', function (err, auction) {
-        if (err) res.send(err);
+      // Use the minimumBid value to create bid instead of the Users max bid (Autobid value)
+      bid.value = minimumBid;
+      msgResult = true;
+      msg = 'Your Autobid for £' + req.body.value + ' has been accepted.';
+    } else {
+      msgResult = false;
+      msg = 'You have already Autobid this amount.';
+    }
+  }
+}
 
-        // removed blocked bids
-        auction = filters.bids(auction, 'blocked');
+const bid = async (req, res) => {
+  try {
+    const userBlocked = await isUserBlocked(req.body.userid)
 
-        // Initialise new standard bid
-        var bid = {
-          value: req.body.value,
-          userid: req.body.userid,
-          email: req.body.email
-        };
-
-        // Return an error if the auction hasn't started yet
-        if (!auction.isValidStartDate()) {
-          return res.send({
-            success: false,
-            message: "Auction is yet to start, you cannot bid yet."
-          });
-        }
-
-        // Return an error if the minimumBid is not met
-        var minimumBid = auction.minimumBid();
-        if (req.body.value < minimumBid) {
-          return res.send({
-            success: false,
-            message: 'The minimum bid is £' + minimumBid
-          });
-        }
-
-        // Check if it's an autobid
-        if (req.body.autobid && req.body.value) {
-          // Check the autobid userid/value don't already exist
-          var autobidExists = auction.autobids.filter(function (obj) {
-            return obj.value == req.body.value && obj.userid == req.body.userid
-          });
-
-          if (autobidExists.length === 0) {
-            // Save the autobid
-            auction.autobids.push({
-              value: req.body.value,
-              userid: req.body.userid,
-              email: req.body.email
-            });
-
-            // Use the minimumBid value to create bid instead of the Users max bid (Autobid value)
-            bid.value = minimumBid;
-            msgResult = true;
-            msg = 'Your Autobid for £' + req.body.value + ' has been accepted.';
-          } else {
-            msgResult = false;
-            msg = 'You have already Autobid this amount.';
-          }
-        }
-
-        // Check if any autobids are in place to override this bid
-        var autobid = auction.checkForAutobid(bid.value);
-        if (autobid.value) {
-          // There's an autobid which is higher than this bid
-          bid.value = autobid.value;
-          bid.userid = autobid.userid;
-          bid.email = autobid.email;
-          // msgResult = false;
-          // msg = 'You have been outbid. Someone had a higher Autobid.'
-        }
-
-        // Check current winner is not this user before we place a bid...
-        var currentWinner = auction.currentWinner() == req.body.userid;
-
-        // Save the bid if not the current winner
-        if (!currentWinner && bid.value > 0) {
-          auction.bids.push(bid);
-          // msg = 'You have the highest bid.';
-        }
-
-        // Save the auction data
-        auction.save(function (err, auction) {
-          if (err) return res.send(err);
-
-          // return the highest bid
-          var highestBid = auction.highestBid();
-
-          res.json({ auction: highestBid, success: true, message: msg, result: msgResult });
-        });
+    if (userBlocked) {
+      return res.send({
+        success: false,
+        message: "Your account has been blocked."
       });
     }
-  });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err
+    })
+  }
+
+  try {
+    const auction = await Auction.findById(req.params.auction_id, 'name description start_date end_date start_amount current_bid countdown _id bids autobids')
+
+    const updatedAuction = await auction.addBid({
+      value: req.body.value,
+      userid: req.body.userid,
+      email: req.body.email
+    })
+
+    res.json({ auction: updatedAuction, success: true });
+  } catch ({ message }) {
+    res.send({
+      success: false,
+      message
+    });
+  }
 }
 
 module.exports = {
