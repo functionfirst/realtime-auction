43a4fe9de402d1c19569e10a44bc4ae3f6b9a383
Alan Jenkins <alanjenkins1978@gmail.com>
2020-02-15 12:19:55 +0000

    Implements try catch to throw authorisation errors

--- app/lib/authorise.js
+++ app/lib/authorise.js
@@ -1,15 +1,19 @@
 // Ensures that the user is an admin
 // Used to restrict access on certain routes
 const auth = (req, res, next) => {
-	// If not admin, restrict access
-	if (!req.admin) {
-		return res.status(403).json({
-			success: false,
-			message: "Restricted access"
-		});
-	}
+	try {
+		if (!req.decoded) {
+			throw new Error('No token provided');
+		}
 
-	next();
+		if (!req.decoded.admin) {
+			throw new Error('Restricted Access.');
+		}
+
+		next();
+	} catch ({ message }) {
+		return res.status(403).json({ message });
+	}
 }
 
 module.exports = auth;
