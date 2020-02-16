09e4e6717fffc4ede2bfca47221b90acd0b1ab98
Alan Jenkins <alanjenkins1978@gmail.com>
2020-02-16 12:12:33 +0000

    De-structures mongoose createConnection method

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const { createConnection } = require('mongoose');
 
 const uristring = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
   `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`
 
-const db = mongoose.createConnection(uristring, {
+const db = createConnection(uristring, {
   useCreateIndex: true,
   useUnifiedTopology: true,
   useNewUrlParser: true
