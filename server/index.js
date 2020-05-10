05dca5f838e8f0a29442806db6d8e995b62c7669
Alan Jenkins <alanjenkins1978@gmail.com>
2020-05-10 12:07:43 +0100

    Moves port setting into env library

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const attachMiddleware = require('./middleware');
 const socketIO = require('./lib/io');
-const port = process.env.PORT || 8080;
+const { port } = require('./lib/env');
 
 // START SERVER
 const server = http.createServer();
--- server/lib/env.js
+++ server/lib/env.js
@@ -1,8 +1,11 @@
 const isProduction = (() => process.env.NODE_ENV === 'production')();
 
+const port = process.env.PORT || 8080
+
 const corsAllowedList = process.env.CORS_ALLOWED_LIST.split(',')
 
 module.exports = {
   corsAllowedList,
-  isProduction
+  isProduction,
+  port
 }
