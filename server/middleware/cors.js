e6a2e26da984aadd217ecd21339546fbb83c8cdb
Alan Jenkins <alanjenkins1978@gmail.com>
2020-05-11 19:26:53 +0100

    Prevent CORS blocking REST tools or server-to-server requests

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -3,7 +3,7 @@ const { corsAllowedList } = require('../lib/env');
 
 const options = {
   origin: function (origin, callback) {
-    if (corsAllowedList.indexOf(origin) !== -1) {
+    if (corsAllowedList.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
