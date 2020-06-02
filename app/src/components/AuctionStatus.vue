26c6922e223ff479cf59d1174fc30535f91fce0b
Alan Jenkins <alanjenkins1978@gmail.com>
2020-06-02 22:11:30 +0100

    Simplifies auction status component
    Leverages date and status helpers.

--- app/src/components/AuctionStatus.vue
+++ app/src/components/AuctionStatus.vue
@@ -1,33 +1,17 @@
 <template>
   <div
-    v-if="auction"
-    class="text-sm mt-2"
+    class="rounded px-2 py-1 text-sm mt-2"
+    :class="`bg-${status.colour}-300`"
   >
-    <div v-if="hasStarted && !hasFinished">
-      <span class="bg-green-300 rounded px-2 py-1">Live</span>
-      This auction started at {{ auction.startDate | formatDate("hh:mma MMM dd, yyyy") }}
-    </div>
-
-    <div v-else-if="hasFinished">
-      <span class="bg-red-300 rounded px-2 py-1">Expired</span>
-      This auction ended at {{ auction.endDate | formatDate("hh:mma MMM dd, yyyy") }}
-    </div>
-
-    <div v-else-if="!hasStarted">
-      Auction starts at {{ auction.startDate | formatDate("hh:mma MMM dd, yyyy") }}
-    </div>
+    {{ status.label }}
   </div>
 </template>
 
 <script>
-import { differenceInSeconds } from "date-fns";
-import { formatDate } from "@/lib/filters";
+import { status } from "@/lib/status";
+import { isInThePast } from "@/lib/dates";
 
 export default {
-  filters: {
-    formatDate
-  },
-
   props: {
     auction: {
       default: () => {},
@@ -37,13 +21,25 @@ export default {
   },
 
   computed: {
+    status() {
+      if (this.hasStarted && !this.hasFinished) {
+        return status.live;
+      } else if (this.hasFinished) {
+        return status.expired;
+      }
+
+      return status.pending;
+    },
+
     hasStarted() {
-      return differenceInSeconds(new Date(), this.auction.startDate) > 0;
+      return isInThePast(this.auction.startDate);
     },
 
     hasFinished() {
-      return differenceInSeconds(new Date(), this.auction.endDate) > 0;
+      return isInThePast(this.auction.endDate);
     }
-  }
+  },
+
+  methods: {}
 };
 </script>
